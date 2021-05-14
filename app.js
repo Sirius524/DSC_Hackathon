74df7fd0e3d24ef973f044cf4a7ad518a5b4ba5f
Hsiang-Ming Hung <bjergsen@Hsiang-MingdeMacBook-Pro.local>
2021-05-15 01:09:22 +0800

    Initail commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
+// 2) Route Handlers
+// 3) Rounte
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
