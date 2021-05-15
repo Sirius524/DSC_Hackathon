a09f45ee5bcffce664754a170111df7a59b666c8
Lexlie <l.exlie823@gmail.com>
2021-05-15 16:47:11 +0800

    Finish user controller

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 // 2) Route Handlers
-// 3) Rounte
+// 3) Route
 
 const certificateRouter = require("./routes/certificateRoutes");
 const userRouter = require("./routes/userRoutes");
--- controllers/userController.js
+++ controllers/userController.js
@@ -35,58 +35,104 @@ var generateToken = () => {
 };
 
 exports.logout = (req, res, next) => {
-  var token = req.header.cookie._token;
-
-  if (token == null) {
+  if (req.headers.cookie == undefined) {
     res.status(200).json({
       status: "error",
       message: "You didn't logged in!",
     });
   }
 
-  User.updateOne({ _token: token }, { _token: "" });
+  var cookies = parseCookie(req.headers.cookie);
+  console.log(cookies);
+  var token = cookies["_token"];
+  console.log(token);
 
-  res
-    .status(200)
-    .json({
+  User.updateOne({ _token: token }, { _token: null }).then(() => {
+    res.status(200).clearCookie("_token").json({
       status: "success",
       message: "Logout successfully!",
-    })
-    .clearCookie("_token");
+    });
+  });
+};
+
+var parseCookie = (cookie) => {
+  var cookies = Array();
+  cookie.split(";").forEach((element) => {
+    element = element.trim().split("=");
+
+    cookies[element[0]] = element[1];
+  });
+
+  return cookies;
 };
 
 exports.registerUser = (req, res, next) => {
-  User.insertMany({ name: req.body.name, email: req.body.email, password: req.body.password }).catch((error) => {
-    res.status(200).json({
-      status: "error",
-      message: "Something went wrong!",
-      log: error,
+  User.create({ name: req.body.name, email: req.body.email, password: req.body.password })
+    .catch((error) => {
+      res.status(200).json({
+        status: "error",
+        message: "Something went wrong!",
+      });
+    })
+    .then(() => {
+      res.status(200).json({
+        status: "success",
+        message: "Registered successfully!",
+      });
     });
-  });
-
-  res.status(200).json({
-    status: "success",
-    message: "Registered successfully!",
-  });
 };
 
 exports.getUserIndex = (req, res, next) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
+  User.find()
+    .select(["name", "email"])
+    .catch((error) => {
+      res.status(200).json({
+        status: "error",
+        message: "Query error!",
+      });
+      console.log(error);
+    })
+    .then((users) => {
+      res.status(200).json({
+        status: "success",
+        message: "Query successfully!",
+        users: users,
+      });
+    });
 };
 
 exports.getUserProfile = (req, res, next) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
+  User.findOne({ _id: ObjectId(req.params.id) })
+    .select(["name", "email"])
+    .catch((error) => {
+      res.status(200).json({
+        status: "error",
+        message: "Query error!",
+      });
+      console.log(error);
+    })
+    .then((user) => {
+      res.status(500).json({
+        status: "success",
+        message: "Query successfully!",
+        user: user,
+      });
+    });
 };
 
 exports.deleteUser = (req, res, next) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
+  User.deleteOne({ _id: ObjectId(req.params.id) })
+    .catch((error) => {
+      res.status(200).json({
+        status: "error",
+        message: "Query error!",
+      });
+      console.log(error);
+    })
+    .then(() => {
+      res.status(200).json({
+        status: "success",
+        message: "Query successfully!",
+      });
+    });
 };
