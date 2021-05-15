23bb45fa008ca6bea37cad0e2ac0b85b179fcfe8
Hsiang-Ming Hung <bjergsen@Hsiang-MingdeMacBook-Pro.local>
2021-05-15 11:53:42 +0800

    db connection

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const DB = require('mongodb+srv://mark:UBEzv5TENYROTgwh@cluster0.z3nm5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const DB = 'mongodb+srv://mark:UBEzv5TENYROTgwh@cluster0.z3nm5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-mongoose.connect();
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(con =>{
+  console.log(con.connection);
+  console.log('DB connect successful!');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
