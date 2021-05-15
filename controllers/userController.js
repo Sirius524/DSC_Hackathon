351e1e9cee8a536ac15af75a2169fbdb5d682b55
Hsiang-Ming Hung <bjergsen@Hsiang-MingdeMacBook-Pro.local>
2021-05-15 14:20:46 +0800

    login function fix success

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,34 +8,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 exports.login = (req, res, next) => {
-  //var user = User.findOne({ email: req.body.email, password: req.body.password });
-  
-  const user = User.findOne({ email: req.body.email }).select('+password');
-  console.log(user);
-  if (!user || req.body.password != user.password) {
-    res.status(200).json({
-      status: "error",
-      message: "Oops! Your email address or password doesn't match our record.",
-    });
-  }
-  
-  // if (user == null) {
-  //   res.status(200).json({
-  //     status: "error",
-  //     message: "Oops! Your email address or password doesn't match our record.",
-  //   });
-  // }
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (req.body.password != user.password) {
+      res.status(200).json({
+        status: "error",
+        message: "Oops! Your email address or password doesn't match our record.",
+      });
+    } else {
+      let token = generateToken();
+      console.log(token);
+      User.updateOne({ _id: user._id }, { _token: token });
 
-  var token = generateToken();
-  User.updateOne({ _id: user._id }, { _token: token });
-
-  res
-    .status(200)
-    .cookie("_token", token, { expires: new Date(Date.now() + 2 * 60 * 60 * 1000) })
-    .json({
-      status: "success",
-      message: "Login success!",
-    });
+      res
+        .status(200)
+        .cookie("_token", token, { expires: new Date(Date.now() + 2 * 60 * 60 * 1000) })
+        .json({
+          status: "success",
+          message: "Login success!",
+        });
+    }
+  });
 };
 
 var generateToken = () => {
