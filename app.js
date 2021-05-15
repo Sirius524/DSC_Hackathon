1c4cd027a432df247511c589965acf13a292321d
billy07001 <72130484+billy07001@users.noreply.github.com>
2021-05-15 20:53:50 +0800

    cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // const fs = require('fs');
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors())
 app.use(morgan("dev"));
 
 app.use(express.json());
