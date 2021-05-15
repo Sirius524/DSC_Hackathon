1fbeaa6bd68d42a331e2b2103a27f629834d7706
Hsiang-Ming Hung <bjergsen@Hsiang-MingdeMacBook-Pro.local>
2021-05-15 11:47:00 +0800

    db connection

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+
+const DB = require('mongodb+srv://mark:UBEzv5TENYROTgwh@cluster0.z3nm5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
+mongoose.connect();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
